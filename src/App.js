da9decbce8e09f8d721839c6b2b0086629b721d8
xtguangqiang <15214313256@xtransfer.cn>
2024-05-31 16:25:11 +0800

    初始化 工程

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const handleButtonClick = () => {
+    console.log('点击了按钮！')
+    window.location.href = 'xtransferapp://xtransferapp';
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +15,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handleButtonClick}>通过Scheme打开APP，新的Scheme是xtransferapp</button>
       </header>
     </div>
   );
